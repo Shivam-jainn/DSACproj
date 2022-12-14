1a3d4c625e681e804ebbe3e8cc60f520930b759b
ravenice <106047819+ravenice@users.noreply.github.com>
2022-12-14 21:06:21 +0530

    Add files via upload

--- phone.c
+++ phone.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 struct node {
     long number;
@@ -32,7 +33,7 @@ void addnode(){
 
     printf("enter number :");
     scanf("%ld",&head->number); 
-
+   
     }else{
 
     current->next = newnode;
@@ -46,6 +47,8 @@ void addnode(){
 
     printf("enter number :");
     scanf("%ld",&newnode->number); 
+     if(head->number<1000000000)
+    printf("enter the valid number ");
 
 
     newnode->prev = current;
@@ -88,10 +91,6 @@ if(current->prev!=NULL){
 
 }
 
-
-
-
-
 void display(){
     struct node* printnode = head;
 
@@ -131,14 +130,60 @@ void searchnode(){
 }
 
 
-void sort(){
-    
+    void sortList() 
+    {  
+    struct node *current = NULL, *index = NULL;  
+    int temp;  
+    //Check whether list is empty  
+    if(head == NULL) {  
+        return;  
+    }  
+    else {  
+        //Current will point to head  
+        for(current = head; current->next != NULL; current = current->next) {  
+            //Index will point to node next to current 
+             for(index = current->next; index != NULL; index = index->next) {  
+                //If current's data is greater than index's data, swap the data of current and index  
+                if(current->number > index->number) {  
+                    temp = current->number;  
+                   current->number = index->number;  
+                   index->number = temp;  
+                }  
+            }  
+        } 
+        display(); 
+    }  
+}  
+    // int comp(char [],char[]);
+    // int i;
+    // char temp[20],s1[20],s2[20];
+    //  struct node* ptr;
+    // for(ptr=head;ptr!=NULL;ptr=ptr->next)
+    // {
+    //     for(ptr=ptr->next;ptr!=NULL;ptr=ptr->next)
+    //     {
+    //         strcpy(s1,ptr->firstname);
+    //         strcpy(s2,ptr->firstname);
+    //         i=comp(s1,s2);
+    //         if(i==1)
+    //         {
+    //             strcpy(temp,ptr->firstname);
+    //             strcpy(ptr->firstname,ptr->firstname);
+    //             strcpy(ptr->firstname,temp);
+    //         }
+    //     }
+    // }
+
+
+int comp(char s1[],char s2[]) {
+    return s1==s2;
 }
+
 
 int main(){
         // M E N U
 while(1){
-    printf("\n MAIN MENU\n 1.Add node \n 2.Delete node \n 3.Display contact list \n4.Search node \n5.Exit");
+    printf("\n MAIN MENU\n 1.Add node \n 2.Delete node \n 3.Display contact list \n4.Search node \n5.Sort\n6.exit");
     int choice;
     scanf("%d",&choice);
 
@@ -162,11 +207,12 @@ while(1){
     break;
     
     case 5:
+    sortList();
+    break;
+    case 6:
     exit(0);
-    break;
-    
     default:printf("enter the right choice\n");
-    break;
+   
     
     }
 
